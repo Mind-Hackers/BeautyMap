4bdb8ed9ebb6b465255f523db721f42f70203580
wei chih tsai <bigdstut@gmail.comm>
2015-09-17 14:35:57 +0800

    update url

--- app/assets/js/services/hotGirlServices.js
+++ app/assets/js/services/hotGirlServices.js
@@ -13,7 +13,8 @@ angular.module('myApp.services')
 
                 getList : function(success,error) {
                     // var apiEndpoint = ServerConfig.endpoint + 'api/hotgirl/list';
-                    var apiEndpoint = 'http://beautymap.bigd.tw/app/libs/angularjs-google-maps/testapp/scripts/starbucks.json';
+                    // var apiEndpoint = 'http://beautymap.bigd.tw/app/libs/angularjs-google-maps/testapp/scripts/starbucks.json';
+                    var apiEndpoint = 'http://isplanet.dyndns-office.com:1880/editor/BeautyMap/app/libs/angularjs-google-maps/testapp/scripts/starbucks.json';
 
                     return $http({
                         url: apiEndpoint,
